ea833060c247f82c90e4bf0d08a9339421781bac
Nicolae Luca <nicolae.luca@student.usv.ro>
2022-11-28 12:48:26 +0200

    juvenil modify

--- src/pages/Juvenil/Juvenil.js
+++ src/pages/Juvenil/Juvenil.js
@@ -1,18 +1,37 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import ".//juvenil.css"
 
 const Juvenil = () => {
   return (
     <>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '90%',
-        }}
-      >
-        <h1>Juvenil</h1>
+  <div class="main-container">
+    <div class="container">
+      <h2>cadet</h2>
+      <div class="btn">
+        <Link to="/cadet">
+        <button class="button" >Cadet</button>
+        </Link>
       </div>
+    </div>
+
+    <div class="container">
+      <h2>junior</h2>
+      <div class="btn">
+        <Link to="/junior">
+        <button class="button" >Junior</button>
+        </Link>
+      </div>
+    </div>
+    <div class="container">
+      <h2>sperante</h2>
+      <div class="btn">
+        <Link to="/sperante">
+        <button class="button" >Sperante</button>
+        </Link>
+      </div>
+    </div>
+  </div>
     </>
   )
 }
---
+++ src/pages/Juvenil/Sperante.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Sperante = () => {
+  return (
+    <>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '90%',
+        }}
+      >
+        <h1>Sperante</h1>
+      </div>
+    </>
+  )
+}
+
+export default Sperante
