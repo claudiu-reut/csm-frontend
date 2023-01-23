c98b4c06151f3440042e21c0a872075b6a86353f
Nicolae Luca <nicolae.luca@student.usv.ro>
2023-01-23 10:23:35 +0200

    istorie

--- src/pages/Detalii/Istorie/Istorie.js
+++ src/pages/Detalii/Istorie/Istorie.js
@@ -1,19 +1,67 @@
 import React from 'react'
 
+import './Istorie.css'
+
 const Istorie = () => {
   return (
-    <>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '90vh',
-        }}
-      >
-        <h1> Istorie</h1>
+    <section id="conference-timeline">
+    <div class="timeline-start">Start</div>
+    <div class="conference-center-line"></div>
+    <div class="conference-timeline-content">
+     
+
+      <div class="timeline-article">
+        <div class="content-left-container">
+          <div class="content-left">
+            <p>Stadionul „Areni” a găzduit un meci internaţional de volei masculin între echipa locală Voinţa şi Selecţionata Sindicală Franceză <span class="article-number">01</span></p>
+          </div>
+        </div>
+        <div class="meta-date">
+          <span class="date">1959</span>
+        </div>
       </div>
-    </>
+      
+
+      <div class="timeline-article">
+        
+        <div class="content-right-container">
+          <div class="content-right">
+            <p>Echipa de volei a Sucevei a promovat în primul eşalon valoric. <span class="article-number">01</span></p>
+          </div>
+        </div>
+        <div class="meta-date">
+          <span class="date">1974</span>
+          
+        </div>
+      </div>
+     
+      <div class="timeline-article">
+        <div class="content-left-container">
+          <div class="content-left">
+            <p>A fost obţinut locul V în campionat şi locul IV în Cupa României. <span class="article-number">01</span></p>
+          </div>
+        </div>
+        <div class="meta-date">
+          <span class="date">1983</span>
+        </div>
+      </div>
+      
+      <div class="timeline-article">
+
+        <div class="content-right-container">
+          <div class="content-right">
+            <p>Promovarea formaţiei în prima ligă. <span class="article-number">02</span></p>
+          </div>
+          
+        </div>
+        <div class="meta-date">
+          <span class="date">2009</span>
+        </div>
+      </div>
+      
+    </div>
+    
+  </section>
   )
 }
 
